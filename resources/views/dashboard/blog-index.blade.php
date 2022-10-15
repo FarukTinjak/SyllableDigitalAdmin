99bc26c6511cd6699936fe2ab963b426d628bcd7
Faruk Tinjak <faruk.tinjak@edu.fit.ba>
2022-10-15 03:56:29 +0200

    Update blog index template

--- resources/views/dashboard/blog-index.blade.php
+++ resources/views/dashboard/blog-index.blade.php
@@ -13,15 +13,13 @@
     <div class="row">
         <div class="col-12">
             <div class="card">
-               {{-- <div class="card-header">
-                    <h3 class="card-title">DataTable with minimal features & hover style</h3>
-                </div>--}}
-                <!-- /.card-header -->
                 <div class="card-body">
-                    <table id="example2" class="table table-bordered table-hover">
+                    <a href="{{route('dashboard.blog.create')}}" class="btn btn-success mb-2"><i class="fa fa-plus mr-1"></i> Create Post</a>
+
+                    <table class="table table-bordered table-hover">
                         <thead>
                         <tr>
-                            <th>Photo</th>
+                            <th class="text-center">Photo</th>
                             <th>Title</th>
                             <th>URL</th>
                             <th>Date</th>
@@ -32,18 +30,18 @@
 @foreach($posts as $post)
                             <tr>
                                 <td class="text-center">
-                                @if($post->thumbnail_path !== null)
-                                    <img src="{{asset('uploads/'.$post->thumbnail_path)}}" style="max-width: 150px; max-height: 150px;">
+                                @if($post->thumbnail_path !== null && file_exists(public_path('uploads/images/' . $post->thumbnail_path)))
+                                    <img src="{{asset('uploads/images/'.$post->thumbnail_path)}}" style="max-width: 150px; max-height: 150px;" alt="">
                                 @else
                                 No photo
                                 @endif
                                 </td>
                                     <td>{{$post->title}}</td>
-                                    <td><a href="/blog/{{$post->slug}}">{{$post->slug}}</a></td>
+                                    <td><a href="/blog/post/{{$post->id}}/{{$post->slug}}">{{$post->slug}}</a></td>
                                     <td>{{$post->created_at}}</td>
                                 <td>
-                                    <a href="" class="btn btn-primary"><i class="fa fa-pencil-alt"></i> Edit</a>
-                                    <a href="" class="btn btn-danger" ><i class="fa fa-trash-alt"></i> Delete</a>
+                                    <a href="{{route('dashboard.blog.edit', ['id' => $post->id])}}" class="btn btn-primary"><i class="fa fa-pencil-alt"></i> Edit</a>
+                                    <a href="{{route('dashboard.blog.delete', ['id' => $post->id])}}" onclick="return confirm('Are you sure you want to delete this record?');" class="btn btn-danger" ><i class="fa fa-trash-alt"></i> Delete</a>
                                 </td>
                             </tr>
 @endforeach

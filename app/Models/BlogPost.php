fb7f81f060f398e3909154ed82b0f6876370b07e
Faruk Tinjak <faruk.tinjak@edu.fit.ba>
2022-10-15 03:52:48 +0200

    Add Laravel plugin

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -5,11 +5,38 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\BlogPost
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $subtitle
+ * @property string $slug
+ * @property string|null $thumbnail_path
+ * @property string $content
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost query()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereThumbnailPath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereUpdatedAt($value)
+ * @mixin \Eloquent
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereSubtitle($value)
+ */
 class BlogPost extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'title',
+        'subtitle',
+        'content',
+        'created_at'
     ];
 }

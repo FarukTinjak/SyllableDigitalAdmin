86358b58483e6886f87e033a44e884b57b0d869b
Faruk Tinjak <faruk.tinjak@edu.fit.ba>
2022-10-15 03:55:00 +0200

    Add create, edit, save and delete methods to BlogController

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,8 +2,8 @@
 namespace App\Http\Controllers;
 use App\Models\BlogPost;
 use Illuminate\Http\Request;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
+use Str;
+
 class BlogController extends Controller
 {
     public function index()
@@ -12,5 +12,56 @@ class BlogController extends Controller
 
         return view('dashboard.blog-index')
             ->with('posts', $posts);
+    }
+
+    public function create() {
+        $post = new BlogPost();
+
+        return view('dashboard.blog-upsert')
+            ->with('post', $post);
+    }
+
+    public function edit($id) {
+        $post = BlogPost::find($id);
+        if(!$post)
+            abort(404);
+
+        return view('dashboard.blog-upsert')
+            ->with('post', $post);
+    }
+    public function save(Request $request)
+    {
+        if(($blog_id = (int)$request->post('id')) != 0){
+            $blog = BlogPost::find($blog_id);
+            if(!$blog)
+                return abort(404);
+        }
+        else {
+            $blog = new BlogPost();
+        }
+        $blog->fill($request->all());
+        $blog->slug = Str::slug($blog->title);
+
+        $destinationPath = public_path('uploads/images');
+
+        if($image = $request->file('thumbnail')) {
+            $imagePath = (microtime(true) * 10000). '.'. strtolower($image->getClientOriginalExtension());
+            $image->move($destinationPath, $imagePath);
+
+            $blog->thumbnail_path = 'uploads/images/' . $imagePath;
+        }
+
+        $blog->save();
+
+        return $blog;
+    }
+    public function delete($id)
+    {
+        $video = BlogPost::find($id);
+        if(!$video)
+            return abort(404);
+
+        $video->delete();
+        return redirect()->route('dashboard.blog');
     }
 }

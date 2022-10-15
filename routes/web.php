e70e5c0694fbfbee9f360894b963042a772f4d9d
Faruk Tinjak <faruk.tinjak@edu.fit.ba>
2022-10-15 03:59:03 +0200

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ImagesController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
@@ -19,7 +20,18 @@ Route::middleware('guest')->group(function() {
 });
 
 Route::middleware('auth')->group(function() {
-    Route::get('dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
+    Route::get('dashboard', function() {
+        return redirect()->route('dashboard.blog');
+    })->name('dashboard');
+
     Route::get('dashboard/blog', [BlogController::class, 'index'])->name('dashboard.blog');
+    Route::get('dashboard/blog/create', [BlogController::class, 'create'])->name('dashboard.blog.create');
+    Route::get('dashboard/blog/edit/{id}', [BlogController::class, 'edit'])->name('dashboard.blog.edit');
+    Route::post('dashboard/blog/save', [BlogController::class, 'save'])->name('dashboard.blog.save');
+    Route::get('dashboard/blog/delete/{id}', [BlogController::class, 'delete'])->name('dashboard.blog.delete');
+
+    Route::post('/images/uploadImageFile', [ImagesController::class, 'uploadImageFile'])->name('images.uploadfile');
+    Route::post('/images/uploadImageUrl', [ImagesController::class, 'uploadImageUrl'])->name('images.uploadurl');
+
     Route::post('signout', [AuthController::class, 'signOut'])->name('signout');
 });
